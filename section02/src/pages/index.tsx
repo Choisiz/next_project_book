0922326674c9de7570e7b710fd445f5097dba35c
최경열 <choisis@choegyeong-yeol-ui-MacBookAir.local>
2025-01-19 19:23:09 +0900

    pageRouter-ssr

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -4,31 +4,40 @@ import SearchLayout from '@/components/searchable-layout';
 import books from '@/mock/books.json'
 import BookItem from '@/components/book-Item';
 import { InferGetServerSidePropsType } from 'next';
+import fetchBooks from '@/lib/fetch-books';
+import fetchRandomBooks from '@/lib/fetch-random-books';
 
 /*ssr */
 //서버측에서 딱한번만 실행되는 함수
 //컴포넌트보다 먼저실행된다.
-export const getServerSideProps = ()=>{
-  const data ='ssr'
+export const getServerSideProps = async()=>{
+  
+  
+  const [allBooks, recoBooks] = await Promise.all([fetchBooks(),fetchRandomBooks()])
   
   return {
     props:{
-      data
+      allBooks,
+      recoBooks
     }
   }
 }
 
 //총 두번실행됨
-export default function Home({data}:InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Home({
+  allBooks,
+  recoBooks
+}:InferGetServerSidePropsType<typeof getServerSideProps>) {
+
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book)=><BookItem key={book.id} {...book}/>)}
+        {recoBooks.map((book)=><BookItem key={book.id} {...book}/>)}
         </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book)=><BookItem key={book.id} {...book}/>)}
+        {allBooks.map((book)=><BookItem key={book.id} {...book}/>)}
         </section>
     </div>
   );
