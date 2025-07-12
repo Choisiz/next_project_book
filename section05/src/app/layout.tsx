bf442e68ed8018ec9839db08ad362681421fa4d8
Choisiz <dnjsvltm201311681@gmail.com>
2025-07-12 19:58:18 +0900

    페이징캐싱

--- section05/src/app/layout.tsx
+++ section05/src/app/layout.tsx
@@ -3,9 +3,12 @@ import Link from "next/link";
 import style from "./layout.module.css";
 import { BookData } from "@/types";
 
+
+
 async function Footer() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
-  
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,{cache:'force-cache'});
+   //force-chche하개 되면서 라우트캐시컴포넌트 즉 캐싱이 된다 해당컴포넌트가 물론 이 api호출은 이제 한번만 하겠지만,
+   //현재 프로젝트에는 크게 상관없다.  
   if(!response.ok){
     return<div>제작 @winterloodss</div>
   }
